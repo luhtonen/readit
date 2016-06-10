1f712fcf1a240f16514d344ab24f95c3b35b271a
luhtonen <luhtonen@gmail.com>
2016-06-10 10:08:56 +0200

    Create Authors list view and template

--- books/views.py
+++ books/views.py
@@ -1,5 +1,7 @@
+from django.db.models import Count
 from django.shortcuts import render
-from .models import Book
+from django.views.generic import View
+from .models import Author, Book
 
 
 # Create your views here.
@@ -17,3 +19,15 @@ def list_books(request):
     }
 
     return render(request, "list.html", context)
+
+
+class AuthorList(View):
+    def get(self, request):
+        authors = Author.objects.annotate(published_books=Count('books'))\
+            .filter(published_books__gt=0)
+
+        context = {
+            'authors': authors,
+        }
+
+        return render(request, "authors.html", context)

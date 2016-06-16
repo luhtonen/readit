8c7323ce9e7203ec1a580764f90a948b737558db
luhtonen <luhtonen@gmail.com>
2016-06-16 09:19:23 +0200

    Update settings and requirements to support production server

--- readit/settings.py
+++ readit/settings.py
@@ -104,6 +104,10 @@ elif DJANGO_MODE == 'staging':
             'PORT': os.getenv('DB_PORT', '5432'),
         }
     }
+elif DJANGO_MODE == 'production':
+    import dj_database_url
+    # Handles DATABASE_URL environment variable
+    DATABASES = {'default': dj_database_url.config()}
 
 
 # Password validation
@@ -143,6 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'readit', 'static'),
